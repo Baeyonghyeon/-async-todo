2a226bf8790e7ee4fcc1f2425c6a1f8394d2274a
kurt <qodydgus97@kakao.com>
2023-03-12 17:12:33 +0900

    init first
    init first

---
+++ asynctodo/src/main/java/com/kurt/asynctodo/AsynctodoApplication.java
@@ -0,0 +1,13 @@
+package com.kurt.asynctodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsynctodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsynctodoApplication.class, args);
+	}
+
+}
---
+++ asynctodo/src/test/java/com/kurt/asynctodo/AsynctodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurt.asynctodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsynctodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
