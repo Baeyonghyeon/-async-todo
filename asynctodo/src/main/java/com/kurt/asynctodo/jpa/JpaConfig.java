5d115093a6ea023d24158c30ddcffa24ce8b6785
kurt <qodydgus97@kakao.com>
2023-05-08 20:51:44 +0900

    Todo 해야할일 추가
    - created By, modified By 는 로그인된 사용자가 떠야하고, 로그인 하지 않으면 default 문자를 넣는다.

---
+++ asynctodo/src/main/java/com/kurt/asynctodo/jpa/JpaConfig.java
@@ -0,0 +1,29 @@
+package com.kurt.asynctodo.jpa;
+
+import com.kurt.asynctodo.security.dto.MemberInfo;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+//        return () -> Optional.ofNullable(SecurityContextHolder.getContext())
+//                .map(SecurityContext::getAuthentication)
+//                .filter(Authentication::isAuthenticated)
+//                .map(Authentication::getPrincipal)
+//                .map(MemberInfo.class::cast)
+//                .map(MemberInfo::username);
+        return () -> Optional.of("kurt");
+    }
+
+}
