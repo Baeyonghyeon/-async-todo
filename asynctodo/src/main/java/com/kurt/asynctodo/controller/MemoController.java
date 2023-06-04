044a8ed0757e221f597c6511484cf28a44aff6bf
kurt <qodydgus97@kakao.com>
2023-06-04 19:25:02 +0900

    Memo CRUD 추가
    비동기 + 논 블로킹 구성

--- asynctodo/src/main/java/com/kurt/asynctodo/controller/MemoController.java
+++ asynctodo/src/main/java/com/kurt/asynctodo/controller/MemoController.java
@@ -1,41 +1,65 @@
 package com.kurt.asynctodo.controller;
 
+import com.kurt.asynctodo.dto.request.MemoRequestDto;
+import com.kurt.asynctodo.dto.response.MemoResponseDto;
+import com.kurt.asynctodo.security.dto.MemberInfo;
 import com.kurt.asynctodo.service.impl.MemoService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/memo")
+@RequestMapping("/memos")
+//Todo : memberInfo 어나니머스 들어올 위험 있음 AOP 처리!!
 public class MemoController {
 
     private final MemoService memoService;
 
-//    @GetMapping
-//    public void getMemoVault(){
-//        memoService.getById(1L);
-//
-//    }
-//
-//    @GetMapping
-//    public void getMemo(){
-//        memoService.getById(1L);
-//    }
-//
-//    @PatchMapping
-//    public void updateMemo(){
-//        memoService.getById(1L);
-//    }
-//
-//    @DeleteMapping
-//    public void deleteMemo(){
-//        memoService.getById(1L);
-//    }
-//
-//    @PostMapping
-//    public void postMemo(){
-//        memoService.getById(1L);
-//    }
+    @GetMapping("/{memo-id}")
+    public ResponseEntity<MemoResponseDto> getMemo(@AuthenticationPrincipal MemberInfo memberInfo,
+                                                   @PathVariable("memo-id") Long memoId) throws ExecutionException, InterruptedException {
+        MemoResponseDto memoResponseDto = memoService.getById(memoId).get();
+
+        return ResponseEntity.ok(memoResponseDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MemoResponseDto>> getMemoList(@AuthenticationPrincipal MemberInfo memberInfo) throws ExecutionException, InterruptedException {
+        List<MemoResponseDto> memoResponseDtoList = memoService.getAll().get();
+
+        return ResponseEntity.ok(memoResponseDtoList);
+    }
+
+
+    @PatchMapping
+    public ResponseEntity<Void> updateMemo(@Valid @RequestBody MemoRequestDto memoRequestDto,
+                           @AuthenticationPrincipal MemberInfo memberInfo) {
+        memoService.updateById(memberInfo, memoRequestDto);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{memo-id}")
+    public void deleteMemo(@AuthenticationPrincipal MemberInfo memberInfo,
+                           @PathVariable("memo-id") Long memoId) {
+        memoService.deleteById(memberInfo, memoId);
+    }
+
+    @PostMapping
+    public ResponseEntity<MemoResponseDto> postMemo(@Valid @RequestBody MemoRequestDto memoRequestDto,
+                         @AuthenticationPrincipal MemberInfo memberInfo) throws ExecutionException, InterruptedException {
+        MemoResponseDto memoResponseDto = memoService.save(memberInfo, memoRequestDto).get();
+
+        return ResponseEntity.created(URI.create("/api/v1/memos/" + memoResponseDto.id()))
+                .body(memoResponseDto);
+    }
 }
--- asynctodo/src/main/java/com/kurt/asynctodo/service/impl/MemoService.java
+++ asynctodo/src/main/java/com/kurt/asynctodo/service/impl/MemoService.java
@@ -1,8 +1,14 @@
 package com.kurt.asynctodo.service.impl;
 
+import com.kurt.asynctodo.converter.MemoConverter;
+import com.kurt.asynctodo.domain.Member;
 import com.kurt.asynctodo.domain.Memo;
+import com.kurt.asynctodo.dto.request.MemoRequestDto;
+import com.kurt.asynctodo.dto.response.MemoResponseDto;
 import com.kurt.asynctodo.repository.MemoRepository;
+import com.kurt.asynctodo.security.dto.MemberInfo;
 import com.kurt.asynctodo.service.CrudService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
@@ -10,45 +16,72 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 @RequiredArgsConstructor
 @Transactional
 @Service
-public class MemoService implements CrudService<Memo> {
+public class MemoService implements CrudService<MemoRequestDto, MemoResponseDto> {
 
     private final MemoRepository memoRepository;
+    private final MemoConverter memoConverter;
 
     @Transactional(readOnly = true)
     @Override
     @Async
-    public CompletableFuture<Memo> getById(final Long id) {
-        return CompletableFuture.completedFuture(null);
+    public CompletableFuture<MemoResponseDto> getById(final Long memoId) {
+        Memo memo = memoRepository.findById(memoId)
+                .orElseThrow(() -> new EntityNotFoundException("This note does not exist."));
+
+        return CompletableFuture.completedFuture(memoConverter.MemoEntityToMomoResponseDto(memo));
     }
 
     @Transactional(readOnly = true)
     @Override
     @Async
-    public CompletableFuture<List<Memo>> getAll() {
-        return CompletableFuture.supplyAsync(memoRepository::findAll);
+    public CompletableFuture<List<MemoResponseDto>> getAll() {
+        List<MemoResponseDto> memoResponseDtoList = memoRepository.findAll().stream()
+                .map(memoConverter::MemoEntityToMomoResponseDto)
+                .toList();
+
+        // Todo : 페이징 적용
+        return CompletableFuture.completedFuture(memoResponseDtoList);
     }
 
     @Override
     @Async
-    public void updateById(final Long id) {
-        // TODO document why this method is empty
+    public void updateById(final MemberInfo memberInfo, final MemoRequestDto requestDto) {
+//        Optional.ofNullable(requestDto.title())
+//                .ifPresent(requestDto::updateTitle);
+//        Optional.ofNullable(requestDto.content())
+//                .ifPresent(requestDto::updateContent);
+
     }
 
     @Override
     @Async
-    public CompletableFuture<Memo> save(final Memo object) {
-        return CompletableFuture.completedFuture(null);
+    public CompletableFuture<MemoResponseDto> save(final MemberInfo memberInfo, final MemoRequestDto requestDto) {
+        Memo memo = Memo.of(null, null, requestDto.title(), requestDto.content());
+        memo.writer(new Member(memberInfo.memberId(), null, null, null, null));
+        Memo save = memoRepository.save(memo);
+
+        return CompletableFuture.completedFuture(memoConverter.MemoEntityToMomoResponseDto(save));
     }
+
 
     @Override
     @Async
-    public void deleteById(Long id) {
-        // TODO document why this method is empty
+    public void deleteById(final MemberInfo memberInfo, Long memoId) {
+        Memo memo = writtenByMeFindMemo(memberInfo, memoId);
+
+        memoRepository.delete(memo);
+    }
+
+    private Memo writtenByMeFindMemo(final MemberInfo memberInfo, final Long inquiryId) {
+        return memoRepository.findById(inquiryId)
+                .filter(a -> Objects.equals(a.getMember().getId(), memberInfo.memberId()))
+                .orElseThrow(() -> new EntityNotFoundException("메모가 존재하지 않거나, 해당 유저가 작성하지 않음."));
     }
 }
